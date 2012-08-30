4a105ea113c6fb7768db78eec128202fa7c4a2bd
Vedat Levi Alev <alev@in.tum.de>
2012-08-31 00:30:08 +0300

    use a list instead of a dict

--- levenshtein.py
+++ levenshtein.py
@@ -8,23 +8,22 @@ def levenshtein(str1, str2):
     # make sure that first string is the smaller one
     if length2 < length1:
         return levenshtein(str2, str1)
-    # maybe i should use nested lists instead of a dictionary
-    edit = {}
+    edit = [list(range(length1 + 1)), [0]*(length2 + 1)] 
     # initialize the matrix
     for i in range(length1 + 1):
-        edit[0,i] = i
+        edit[0][i] = i
     curr, prev = 0, 1
     for i in range(1,length2 + 1):
         # toggle the two lines
         curr, prev = prev, curr
-        edit[curr,0] = i
+        edit[curr][0] = i
         for j in range(1,length1 + 1):
             # the cost of the substitution
             diff = 0 if str1[j - 1] == str2[i - 1] else 1
-            edit[curr, j] = min(
-                                 edit[prev, j]     + 1,   # deletion
-                                 edit[curr, j - 1] + 1,   # insertion
-                                 edit[prev, j - 1] + diff # substitution
+            edit[curr][j] = min(
+                                 edit[prev][j]     + 1,   # deletion
+                                 edit[curr][j - 1] + 1,   # insertion
+                                 edit[prev][j - 1] + diff # substitution
                                )
-    return edit[curr, length1]
+    return edit[curr][length1]
     
